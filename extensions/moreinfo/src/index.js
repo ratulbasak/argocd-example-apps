36e659f133cf729a78fed2c50f08d22837285491
ratulbasak <ratulbasak93@gmail.com>
2025-01-26 23:34:38 +0100

    added extension

--- extensions/moreinfo/src/index.js
+++ extensions/moreinfo/src/index.js
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { useEffect, useState } from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
-// import ResourceCard from './Resources/Card/Card';
-
 
 const ResourceCard = ({ title, value }) => {
     return (
@@ -15,25 +13,27 @@ const ResourceCard = ({ title, value }) => {
     );
 };
 
-export const MoreInfo = ( props ) => {
+export const MoreInfo = (props) => {
     const [metrics, setMetrics] = useState({
         totalResources: 0,
         totalPods: 0,
         outOfSyncCount: 0,
     });
 
-    // const { application, tree } = props;
-    const application_name = props.application.metadata.name || "";
+    // Destructure application and tree from props
+    const { application, tree } = props;
+
+    // Safely access application name
+    const application_name = application?.metadata?.name || "";
 
     console.log("appname: ", application_name);
     console.log("props: ", props);
 
-
     useEffect(() => {
-        if (props) {
-            const totalResources = props.application.status.resources.length;
-            const totalPods = props.tree.nodes.filter(r => r.kind === 'Pod').length;
-            const outOfSyncCount = props.application.status.resources.filter(r => r.status !== 'Synced').length;
+        if (application?.status && tree?.nodes) {
+            const totalResources = application.status.resources.length;
+            const totalPods = tree.nodes.filter(r => r.kind === 'Pod').length;
+            const outOfSyncCount = application.status.resources.filter(r => r.status !== 'Synced').length;
 
             setMetrics({
                 totalResources,
@@ -41,29 +41,26 @@ export const MoreInfo = ( props ) => {
                 outOfSyncCount,
             });
         }
-    }, [props]);
+    }, [application, tree]);
 
     console.log("more_info: ", metrics);
 
     return (
-      // <React.Fragment>
         <div style={{ display: 'flex', flexWrap: 'wrap' }}>
             <ResourceCard title="Resources" value={metrics.totalResources || 0} />
             <ResourceCard title="Pods" value={metrics.totalPods || 0} />
             <ResourceCard title="Out-of-Sync" value={metrics.outOfSyncCount || 0} />
         </div>
-      // </React.Fragment>
     );
 };
 
 export const component = MoreInfo;
 
-
 ((window) => {
-  window.extensionsAPI.registerResourceExtension(
-    component,
-    "argoproj.io",
-    "Application",
-    "moreinfo"
-  );
+    window.extensionsAPI.registerResourceExtension(
+        component,
+        "argoproj.io",
+        "Application",
+        "moreinfo"
+    );
 })(window);
