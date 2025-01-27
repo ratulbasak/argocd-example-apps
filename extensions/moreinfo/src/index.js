428596a415dfcfc4d9d031598351d582d58a7f2a
ratulbasak <ratulbasak93@gmail.com>
2025-01-27 01:27:42 +0100

    added extension

--- extensions/moreinfo/src/index.js
+++ extensions/moreinfo/src/index.js
@@ -1,27 +1,57 @@
 import * as React from "react";
 import { useEffect, useState } from 'react';
-import './index.css';
-// import { Card, CardContent, Typography } from '@mui/material';
 
-// const ResourceCard = ({ title, value }) => {
-//     return (
-//         <Card variant="outlined" style={{ margin: '10px' }}>
-//             <CardContent>
-//                 <Typography variant="h5">{title}</Typography>
-//                 <Typography variant="body1">{value}</Typography>
-//             </CardContent>
-//         </Card>
-//     );
-// };
 
 const MetricItem = ({ title, value }) => {
+    const itemStyle = {
+        width: "60px",
+        flexGrow: 1,
+        color: "#495763",
+        textAlign: "center",
+        margin: "5px",
+        padding: "10px",
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        backgroundColor: "#f9f9f9",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    };
+
+    const titleStyle = {
+        display: "block",
+        fontWeight: "bold",
+    };
+
+    const valueStyle = {
+        display: "block",
+        fontSize: "1.2em",
+    };
+
     return (
-        <div className="metric-item">
-            <span className="metric-title">{title}</span>
-            <span className="metric-value">{value}</span>
+        <div style={itemStyle}>
+            <span style={titleStyle}>{title}</span>
+            <span style={valueStyle}>{value}</span>
         </div>
     );
 };
+
+const MetricsDisplay = ({ metrics }) => {
+    const containerStyle = {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        padding: '10px',
+    };
+
+    return (
+        <div style={containerStyle}>
+            <MetricItem title="Resources" value={metrics.totalResources || 0} />
+            <MetricItem title="Pods" value={metrics.totalPods || 0} />
+            <MetricItem title="Out-of-Sync" value={metrics.outOfSyncCount || 0} />
+        </div>
+    );
+};
+
+export default MetricsDisplay;
 
 export const MoreInfo = (props) => {
     const [metrics, setMetrics] = useState({
@@ -56,44 +86,7 @@ export const MoreInfo = (props) => {
     console.log("more_info: ", metrics);
 
     return (
-      <div className="metrics-container">
-            <MetricItem title="Resources" value={metrics.totalResources || 0} />
-            <MetricItem title="Pods" value={metrics.totalPods || 0} />
-            <MetricItem title="Out-of-Sync" value={metrics.outOfSyncCount || 0} />
-      </div>
-      // <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      //     <div style={{
-      //         width: "60px",
-      //         flexGrow: "1",
-      //         color: "#495763",
-      //         textAlign: "center",
-      //       }}
-      //     >
-      //       Resources {metrics.totalResources || 0}
-      //     </div>
-      //     <div style={{
-      //         width: "60px",
-      //         flexGrow: "1",
-      //         color: "#495763",
-      //         textAlign: "center",
-      //       }}
-      //     >
-      //       Pods {metrics.totalPods || 0}
-      //     </div>
-      //     <div style={{
-      //         width: "60px",
-      //         flexGrow: "1",
-      //         color: "#495763",
-      //         textAlign: "center",
-      //       }}
-      //     >
-      //       Out-of-Sync {metrics.outOfSyncCount || 0}
-      //     </div>
-      //     {/* <ResourceCard title="Resources" value={metrics.totalResources || 0} />
-      //     <ResourceCard title="Pods" value={metrics.totalPods || 0} />
-      //     <ResourceCard title="Out-of-Sync" value={metrics.outOfSyncCount || 0} /> */}
-      // </div>
-      
+      <MetricsDisplay metrics={metrics} />
     );
 };
 
